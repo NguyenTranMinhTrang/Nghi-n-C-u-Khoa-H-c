3071b4e3505b2174d6978f782b971d16b1f8deb7
NguyenTranMinhTrang <minhtrang.9096@gmail.com>
2022-01-13 14:17:07 +0700

    Fix View Login

--- src/views/login.js
+++ src/views/login.js
@@ -2,10 +2,8 @@ import {
     StyleSheet,
     Text,
     View,
-    ScrollView,
     TextInput,
     SafeAreaView,
-    ImageBackground,
     Dimensions,
     Image,
     TouchableOpacity
@@ -13,31 +11,31 @@ import {
 import React, { Component, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Button from '../components/button';
 
-const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
-
 
 export default function Login({navigation}) {
     const [visable, setVisable] = useState(true);
     return (
-        <View style={{ width: '100%', height: '100%' , backgroundColor : 'powderblue'}} >
-            <SafeAreaView style={{ flex: 1 , alignItems : 'center'}}>
-                <View style={{width : '40%', height : '20%' , backgroundColor : 'powderblue' , marginTop : 0.1*windowHeight, alignItems : 'center', justifyContent: 'center'}} >
-                    <Image source={require('../images/imgStart/appIcon.png') } 
-                        style={{ height: '100%', width: '100%' , opacity :1}}
+        <View style={{ width: '100%', height: '100%', backgroundColor: 'powderblue' }} >
+            <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+                <View style={{ width: '40%', height: '20%', backgroundColor: 'powderblue', marginTop: 0.05 * windowHeight, alignItems: 'center', justifyContent: 'center' }} >
+                    <Image source={require('../images/imgStart/appIcon.png')}
+                        style={{ height: '100%', width: '100%', opacity: 1 }}
                         resizeMode='stretch'
                     />
                 </View>
 
-                <View style={{ width: '100%', height: '40%', marginTop: 10, alignItems: 'center' }}>
-                    {/* Username */}
-                    <View style={styles.input}>
+                <View style={{ height: '40%', width: '85%', marginTop: 20 }}>
+                    {/* Username Input */}
+                    <View style={[styles.input, styles.spacer10]}>
                         <TextInput style={styles.text} placeholder='UserName' />
                     </View>
+
                     {/* Password */}
-                    <View style={styles.inputPassword}>
-                        <TextInput style={styles.password} placeholder='Password' secureTextEntry={visable && true} />
+                    <View style={[styles.input, styles.spacer20]}>
+                        <TextInput style={[styles.text, styles.inputPassword]} placeholder='Password' secureTextEntry={visable && true} />
                         <TouchableOpacity
                             style={{ height: '100%', width: 50, position: 'absolute', right: 0 }}
                             onPress={() => {
@@ -47,7 +45,7 @@ export default function Login({navigation}) {
                             {visable === false ? <Image source={require('../images/imgLogin/eyeClose.png')}
                                 style={{ height: '100%', width: '100%' }}
                                 resizeMode='stretch'
-                                /> :
+                            /> :
                                 <Image source={require('../images/imgLogin/eye.png')}
                                     style={{ height: '100%', width: '100%' }}
                                     resizeMode='stretch'
@@ -56,46 +54,29 @@ export default function Login({navigation}) {
                     </View>
 
                     {/* Button Login */}
-
-                    <TouchableOpacity style={styles.button} onPress={() => {navigation.navigate('Home')}}>
-                        <Text style={{ fontSize: 25, color: 'black', fontWeight: 'bold' }}>Login</Text>
-                    </TouchableOpacity>
-
+                    <Button onPress={() => navigation.navigate('Home')}>
+                        <Text style={styles.buttonText}>Login</Text>
+                    </Button>
                 </View>
 
                 {/* Button Register */}
-                <View style={{ width: '100%', height: '10%', marginTop: 0.15 * windowHeight, alignItems: 'center' }}>
-                    <TouchableOpacity style={styles.buttonRegister}>
-                        <Text style={{ fontSize: 25, color: 'black', fontWeight: 'bold' }}>Create an account</Text>
-                    </TouchableOpacity>
-                </View>
+                <Button style={styles.buttonRegister}>
+                    <Text style={styles.buttonText}>Create an account</Text>
+                </Button>
             </SafeAreaView>
         </View>
     )
 }
-
 
 const styles = StyleSheet.create({
     input: {
         backgroundColor: 'white',
         borderWidth: 2,
         height: '25%',
-        width: '85%',
+        width: '100%',
         borderRadius: 25,
         alignItems: 'center',
         borderColor: 'darkcyan',
-    },
-
-    inputPassword: {
-        backgroundColor: 'white',
-        borderWidth: 2,
-        height: '25%',
-        width: '85%',
-        borderRadius: 25,
-        alignItems: 'center',
-        marginTop: 10,
-        borderColor: 'darkcyan',
-
     },
 
     text: {
@@ -104,34 +85,27 @@ const styles = StyleSheet.create({
         fontSize: 20
     },
 
-    password: {
-        height: '100%',
-        width: '80%',
-        fontSize: 20,
+    inputPassword: {
         paddingRight: 40,
-
     },
 
-    button: {
-        borderWidth: 2,
-        height: '25%',
-        width: '85%',
-        borderRadius: 25,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 30,
-        borderColor: 'floralwhite',
+    spacer10: {
+        marginBottom: 10,
+    },
+
+    spacer20: {
+        marginBottom: 20,
+    },
+
+    buttonText: {
+        fontSize: 25,
+        color: 'black',
+        fontWeight: 'bold',
     },
 
     buttonRegister: {
-        borderWidth: 2,
-        height: '100%',
-        width: '85%',
-        borderRadius: 25,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderColor: 'floralwhite',
+        height : '10%' , 
+        width : '85%', 
+        marginTop: 0.15 * windowHeight,
     }
-});
+})
